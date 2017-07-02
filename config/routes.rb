f4e1200c4ad284d2a348090df3faca1c3f2a87a8
adam-szczombrowski <adam.szczombrowski@gmail.com>
2017-07-02 17:02:28 +0200

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
-  get 'react_examples/component', to: 'react_examples#component', as: :component
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :appointments, only: [:index, :create]
 end
