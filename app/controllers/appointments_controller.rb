008509ca84f3e77d8e3b51ba766d1af736e1c9bc
adam-szczombrowski <adam.szczombrowski@gmail.com>
2017-07-02 17:03:15 +0200

    add appointments controller

---
+++ app/controllers/appointments_controller.rb
@@ -0,0 +1,18 @@
+class AppointmentsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def index
+    @appointments = Appointment.all
+  end
+
+  def create
+    @app = Appointment.create(appointment_params)
+    render json: @app
+  end
+
+  private
+
+  def appointment_params
+    params.require(:appointment).permit(:name, :date)
+  end
+end
